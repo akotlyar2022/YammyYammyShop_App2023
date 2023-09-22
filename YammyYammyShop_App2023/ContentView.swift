8d35e15f99be410fdbc0e0e4db466bdab318e1ff
AleksandrKotliar <sashagsmnew@gmail.com>
2023-09-22 19:26:12 +0400

    Create Authentification Screen, Text fields, Buttons. Add additional buttons and animations

--- YammyYammyShop_App2023/ContentView.swift
+++ YammyYammyShop_App2023/ContentView.swift
@@ -8,14 +8,94 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isAuth = true
+    
+    @State private var email = ""
+    @State private var password = ""
+    @State private var confirmPassword = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        VStack(spacing: 48) {
+            Text("YammyYammy")
+                .padding()
+                .padding(.horizontal, 15)
+                .padding(.top, 50)
+                //.font(.largeTitle.bold())
+                .font(.system(size: 45, weight: .bold))
+                //.background(Color("whiteAlfa"))
+                .cornerRadius(20)
+                .foregroundColor(.red)
+            
+            Text(isAuth ? "Authorization" : "Registration")
+                .padding(isAuth ? 16 : 24)
+                .padding(.horizontal, 30)
+                //.padding(.top, 50)
+                .font(.title.bold())
+                .background(Color("whiteAlfa"))
+                .cornerRadius(isAuth ? 20 : 40)
+                .foregroundColor(isAuth ? .blue : .green)
+            VStack {
+                TextField("Enter e-mail", text: $email)
+                    .padding()
+                    .background(Color("whiteAlfa"))
+                    .cornerRadius(15)
+                    .padding(4)
+                    .padding(.horizontal, 12)
+                SecureField("Enter password", text: $password)
+                    .padding()
+                    .background(Color("whiteAlfa"))
+                    .cornerRadius(15)
+                    .padding(4)
+                    .padding(.horizontal, 12)
+                
+                if !isAuth {
+                    SecureField("Enter password again", text: $confirmPassword)
+                        .padding()
+                        .background(Color("whiteAlfa"))
+                        .cornerRadius(15)
+                        .padding(4)
+                        .padding(.horizontal, 12)
+                }
+                
+                Button {
+                    print("Authorization")
+                } label: {
+                    Text(isAuth ? "Enter" : "Create account")
+                        .font(.title2.bold())
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        //.background(Color("orange"))
+                        .cornerRadius(15)
+                        .padding(10)
+                        .padding(.horizontal, 12)
+                }
+                Button {
+                    isAuth.toggle()
+                } label: {
+                    Text(isAuth ? "No account?" : "Have account")
+                        .font(.title2.bold())
+                        .padding(.horizontal)
+                        .frame(maxWidth: .infinity)
+                        //.background(Color("orange"))
+                        .cornerRadius(15)
+                        .padding(10)
+                        .padding(.horizontal, 12)
+                }
+                
+            }
+            .padding()
+            .padding(.top, 20)
+            .background(Color("whiteAlfa"))
+            .cornerRadius(24)
+            .padding(isAuth ? 30 : 12)
+            
+            
+        }.frame(maxWidth: .infinity,  maxHeight: .infinity)
+            .background(Image("yammyBG").ignoresSafeArea()
+                .blur(radius: isAuth ? 0 : 6)
+            )
+            .animation(Animation.easeInOut(duration: 0.4), value: isAuth)
     }
 }
 
