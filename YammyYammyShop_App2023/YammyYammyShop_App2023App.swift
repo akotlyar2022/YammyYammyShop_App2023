a68c825498f5fff665b4236e0d1669ca15d9fdf3
AleksandrKotliar <sashagsmnew@gmail.com>
2023-09-22 13:13:05 +0400

    Initial Commit

---
+++ YammyYammyShop_App2023/YammyYammyShop_App2023App.swift
@@ -0,0 +1,17 @@
+//
+//  YammyYammyShop_App2023App.swift
+//  YammyYammyShop_App2023
+//
+//  Created by macbook on 22.09.23.
+//
+
+import SwiftUI
+
+@main
+struct YammyYammyShop_App2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
