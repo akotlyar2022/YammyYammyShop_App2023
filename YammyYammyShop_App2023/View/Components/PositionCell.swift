51ca08b40b42c99cbde5a060716830db4cdb2fc9
AleksandrKotliar <sashagsmnew@gmail.com>
2023-09-23 22:31:42 +0400

    Create PositionCell. Added elements to CartView

---
+++ YammyYammyShop_App2023/View/Components/PositionCell.swift
@@ -0,0 +1,18 @@
+//
+//  PositionCell.swift
+//  YammyYammyShop_App2023
+//
+//  Created by macbook on 23.09.23.
+//
+
+import SwiftUI
+
+struct PositionCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PositionCell()
+}
