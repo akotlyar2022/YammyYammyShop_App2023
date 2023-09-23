cc1de4267b0546f2f0cc12068af36109b1b9fe55
AleksandrKotliar <sashagsmnew@gmail.com>
2023-09-23 20:18:51 +0400

    Create Position, CartViewModel, ProductDetailViewModel. Added some elements to views

---
+++ YammyYammyShop_App2023/Model/Position.swift
@@ -0,0 +1,8 @@
+//
+//  Position.swift
+//  YammyYammyShop_App2023
+//
+//  Created by macbook on 23.09.23.
+//
+
+import Foundation
---
+++ YammyYammyShop_App2023/ViewModel/CartViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CartViewModel.swift
+//  YammyYammyShop_App2023
+//
+//  Created by macbook on 23.09.23.
+//
+
+import Foundation
