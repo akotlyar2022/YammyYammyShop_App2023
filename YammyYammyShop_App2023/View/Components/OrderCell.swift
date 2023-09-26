63dc6071d42947b7417e3e11131da14f5420c0a2
AleksandrKotliar <sashagsmnew@gmail.com>
2023-09-27 00:02:20 +0400

    Create OrderCell. Added functionality for receiving and displaying orders in Firebase Firestore. Test orders ok

---
+++ YammyYammyShop_App2023/View/Components/OrderCell.swift
@@ -0,0 +1,18 @@
+//
+//  OrderCell.swift
+//  YammyYammyShop_App2023
+//
+//  Created by macbook on 26.09.23.
+//
+
+import SwiftUI
+
+struct OrderCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderCell()
+}
